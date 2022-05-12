af5a49d027583d012b2bc0fa03f130a32e456ff2
steph45607 <87968521+steph45607@users.noreply.github.com>
2022-05-12 23:21:29 +0700

    Add storage

---
+++ ForumWeek9/Stroage.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Storage {
+    static List<String> banks = new ArrayList<>();
+    static Map<String, Bank> bankExists = new HashMap<>();
+
+}
