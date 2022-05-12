d59af91d0a02b2dc01d61e088bae247133ebc6e5
steph45607 <87968521+steph45607@users.noreply.github.com>
2022-05-12 23:19:30 +0700

    Add forum week 9 solution and driver

---
+++ ForumWeek9/Driver.java
@@ -0,0 +1,23 @@
+public class Driver {
+    public static void main(String[] args){
+        System.out.println("        _.^._                       _.^._");
+        System.out.println("       _|   |_                     _|   |_");
+        System.out.println("       | ... |_._._._._._._._._._._| ... |");
+        System.out.println("       | ||| |  o BANK  FACTORY o  | ||| |");
+        System.out.println("       | \"\"\" |  \"\"\"    \"\"\"    \"\"\"  | \"\"\" |");
+        System.out.println("  ())  |[-|-]| [-|-]  [-|-]  [-|-] |[-|-]|  ())");
+        System.out.println(" (())) |     |---------------------|     | (()))");
+        System.out.println("(())())| \"\"\" |  \"\"\"    \"\"\"    \"\"\"  | \"\"\" |(())())");
+        System.out.println("(()))()|[-|-]|  :::   .-\"-.   :::  |[-|-]|(()))()");
+        System.out.println("()))(()|     | |~|~|  |_|_|  |~|~| |     |()))(()");
+        System.out.println("   ||  |_____|_|_|_|__|_|_|__|_|_|_|_____|  ||");
+        System.out.println("~ ~^^ @@@@@@@@@@@@@@/=======\\@@@@@@@@@@@@@@ ^^~ ~");
+        System.out.println("     ^~^~                                ~^~^");
+
+        System.out.println("--------------Welcome to Bank Factory--------------");
+        System.out.println("-------You can go to or create your own bank-------");
+        Methods.welcomeBank();
+
+    }
+
+}
